a026f11edce673c14e96cda65c9f71b231cc527c
Tyler Renslow <tyler.renslow@medel.com>
2023-06-21 19:09:10 +0200

    Challenge 0: Convert a notebook to production code

--- src/model/train.py
+++ src/model/train.py
@@ -4,6 +4,7 @@ import argparse
 import glob
 import os
 
+import mlflow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
@@ -12,7 +13,7 @@ from sklearn.linear_model import LogisticRegression
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,7 +35,14 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    # split data
+    X_train = df.drop("Diabetic", axis=1)
+    X_test = df["Diabetic"]
+    y_train = df.drop("Diabetic", axis=1)
+    y_test = df["Diabetic"]
 
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
